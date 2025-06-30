fdd21cffa029e2608c246f6633d9f73f6231aaec
pedrobsantos21 <pedro.santos@detran.sp.gov.br>
2025-06-30 17:14:56 -0300

    build: Create script to generate sysdata.rda\n\nThis commit introduces a script to process raw data and generate the `sysdata.rda` file. The script reads data from `tb_municipio.csv` and `RELATORIO_DTB_BRASIL_MUNICIPIO.xls`, filters for municipalities in São Paulo, and saves the resulting data frames to be used by the package.

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,3 +1,12 @@
-## code to prepare `sysdata` dataset goes here
+path_municipios = "data-raw/tb_municipio.csv"
+municipios = readr::read_csv(path_municipios)
 
-usethis::use_data(sysdata, overwrite = TRUE)
+path_list_ibge = "data-raw/RELATORIO_DTB_BRASIL_MUNICIPIO.xls"
+list_ibge = readxl::read_excel(path_list_ibge, skip = 6)
+
+list_ibge_sp = list_ibge |>
+    janitor::clean_names() |>
+    dplyr::filter(nome_uf == "São Paulo") |>
+    dplyr::select(cod_ibge = codigo_municipio_completo, nome_municipio)
+
+save(list_ibge_sp, municipios, "../R/sysdata.rda")
